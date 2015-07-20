065f6d02b400099a36d950e1cf75a90d34cba87f
Martin Flodin <mflodin@gmail.com>
2015-07-20 21:24:58 +0200

    Finished step 3

--- client/app/components/blog/blog.controller.js
+++ client/app/components/blog/blog.controller.js
@@ -1,3 +1,20 @@
-// TODO: create the blog controller
-// check blog.html for hints on what the controller
-// should have/do
+// create our controller using the
+// highly controversial class
+import _ from 'lodash';
+
+class BlogController {
+  // bind to this and not $scope
+  // because of controllerAs.
+  constructor() {
+    this.message = 'Kittens!';
+    this.posts = _.times(9, i => {
+      return {
+      	title: 'Kitten',
+      	author: 'Mr Meowgi'
+      };
+    });
+  }
+}
+// could also just export the class up top as well
+export {BlogController};
+
